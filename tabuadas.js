4df832e24132bc2088760b1a6cc3c73b100fe712
Daniel Sonobe Silveira <dev.obe78@gmail.com>
2022-04-29 22:58:29 -0300

    tabuadas-js

---
+++ tabuadas.js
@@ -0,0 +1,15 @@
+let operator = 5;
+
+for (let i = 1; i < 11; i++) {
+
+    console.log(i + ' x ' + ' = ' + (operator * i));
+    /* console.log (`Tabuada do ${i}`); */
+
+    for (let h = 1; h < 11; h++) {
+
+        /* console.log(h + ' x ' + i + ' = ' + (i * h)); */
+
+    }
+    
+}
+

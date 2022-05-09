0793f64d46a86fad743ed2feb2e0af9c5d74348c
Daniel Sonobe Silveira <dev.obe78@gmail.com>
2022-05-09 20:37:03 -0300

    usando fizzBuzz na função

---
+++ Laboratorio-JavaScript/class7-fizzBuzz.js
@@ -0,0 +1,27 @@
+/* 
+* Divisível por 3 => fizz; 
+* Divisível por 5 => buzz; 
+* Divisível por 3 e 5 => fizzBuzz; 
+* Não é um número => Não é um número; 
+*/
+
+const resultado = fizzBuzz (25);
+console.log (resultado);
+
+function fizzBuzz (entrada) {
+
+    if (typeof entrada !== 'number')
+        return 'Não é um número';
+   
+    if ((entrada % 3 === 0) && (entrada % 5 === 0))
+        return 'FizzBuzz';
+
+    if (entrada % 3 === 0)
+     return 'Fizz';
+
+    if (entrada % 5 === 0)
+     return 'Buzz';
+    
+    
+    return entrada; 
+}
